026e8d436ae167be1005c66d3e97eb5b619bd2e2
Vaishnav Negi <41587744+vaishnavnegi@users.noreply.github.com>
2023-12-21 00:39:42 +0100

    Create tests.sh
    This bash script runs the data pipeline and removes any preloaded output files if they exist, before executing the pipeline. It then validates the existence of required output files and gives appropriate messages to signal success or failure.

---
+++ project/tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Define output file paths
+output_file_it20="../data/ball_by_ball_it20.csv"
+output_file_ipl="../data/ball_by_ball_ipl.csv"
+
+# Remove existing output files if they exist
+rm -f "$output_file_it20"
+rm -f "$output_file_ipl"
+
+# Execute the data pipeline
+python3 /path/to/Project/pipeline.py
+
+# Validate the output files
+if [ -f "$output_file_it20" ]; then
+    echo "IT20 output file exists: $output_file_it20"
+else
+    echo "IT20 output file not found: $output_file_it20"
+    exit 1
+fi
+
+if [ -f "$output_file_ipl" ]; then
+    echo "IPL output file exists: $output_file_ipl"
+else
+    echo "IPL output file not found: $output_file_ipl"
+    exit 1
+fi
+
+echo "Tests passed successfully!"
